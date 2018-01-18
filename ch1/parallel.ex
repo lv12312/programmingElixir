9c1ad16c82e0e02ae8dedcac8580bf27c9292cf0
lv12312 <lv-12312@163.com>
2018-01-18 22:37:33 +0800

    add hello.

---
+++ ch1/parallel.ex
@@ -0,0 +1,8 @@
+defmodule Parallel do
+  @moduledoc false
+  def pmap(collection, func) do
+    collection
+    |> Enum.map(&(Task.async(fn -> func.(&1) end)))
+    |> Enum.map(&Task.await/1)
+  end
+end
